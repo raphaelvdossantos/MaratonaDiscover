bef878119a60bfed114281709adc54ee77768e92
raphaelvdossantos <rvsantos97@gmail.com>
2021-02-17 01:04:44 -0300

    Solved Graph Bug

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const Modal = {
       .classList.remove('active');
     document
       .querySelector('#form')
-        .classList.remove('hide')
+      .classList.remove('hide')
     document.querySelector('.downloadbox')
       .classList.add('hide')
   },
@@ -22,38 +22,38 @@ const Modal = {
       .classList
       .remove("active");
   },
-  openEdit(){
+  openEdit() {
     document
-    .querySelector(".modal-overlay")
-    .classList
-    .add("active");
-  document
-    .querySelector(".error-box")
-    .classList.remove('active');
-  document
-    .querySelector('#form')
+      .querySelector(".modal-overlay")
+      .classList
+      .add("active");
+    document
+      .querySelector(".error-box")
+      .classList.remove('active');
+    document
+      .querySelector('#form')
       .classList.remove('hide')
-  document.querySelector('.downloadbox')
-    .classList.add('hide')
+    document.querySelector('.downloadbox')
+      .classList.add('hide')
   }
 };
 
 const Storage = {
-  get(){
+  get() {
     return JSON
       .parse(localStorage
         .getItem("dev.finances:transactions")) || [];
   },
-  set(transactions){
+  set(transactions) {
     localStorage
       .setItem("dev.finances:transactions", JSON.stringify(transactions));
   },
-  getMode(){
+  getMode() {
     return JSON
       .parse(localStorage
         .getItem("Mode:modeTheme")) || [];
   },
-  setMode(theme){
+  setMode(theme) {
     localStorage
       .setItem("Mode:modeTheme", JSON.stringify(theme));
   }
@@ -62,29 +62,29 @@ const Storage = {
 const Transaction = {
   all: Storage.get(),
   underEditionIndex: null,
-  add(transaction){
+  add(transaction) {
     Transaction.all.push(transaction);
     App.reload();
   },
-  saveEdit(transaction, index){
+  saveEdit(transaction, index) {
     Transaction.all[index] = transaction;
     App.reload();
   },
-  remove(index){
+  remove(index) {
     Transaction.all.splice(index, 1);
     App.reload();
   },
-  incomes(){
+  incomes() {
     let income = 0;
     Transaction.all.forEach(element => element.amount > 0 ? income += element.amount : income);
     return income;
   },
-  expenses(){
+  expenses() {
     let expense = 0;
     Transaction.all.forEach(element => element.amount < 0 ? expense += element.amount : expense);
     return expense;
   },
-  total(){
+  total() {
     let total = 0;
     Transaction.all.forEach(element => element.amount ? total += element.amount : total);
     return Transaction.incomes() + Transaction.expenses();
@@ -92,7 +92,7 @@ const Transaction = {
 }
 
 const PanelControl = {
-  changePanel(){
+  changePanel() {
     const calculatorPanel = document.querySelector('#calculator');
     const chartPanel = document.querySelector('.chart-page');
     calculatorPanel.classList.toggle('hide');
@@ -109,7 +109,7 @@ const DOM = {
 
     DOM.transactionContainer.appendChild(tr);
   },
-  innerHTMLTransaction(transaction, index){
+  innerHTMLTransaction(transaction, index) {
     const { description, amount, date } = transaction;
 
     const transactionType = amount > 0 ? 'income' : 'expense';
@@ -128,11 +128,11 @@ const DOM = {
 
     return html
   },
-  editTransaction(index){
+  editTransaction(index) {
     Modal.isEdit = true;
     Modal.openEdit();
     document.querySelector('input#description').value = Transaction.all[index].description
-    document.querySelector('input#amount').value = Transaction.all[index].amount/100
+    document.querySelector('input#amount').value = Transaction.all[index].amount / 100
     document.querySelector('input#date').value = Utils.reformatDate(Transaction.all[index].date)
 
     Transaction.underEditionIndex = index;
@@ -148,43 +148,43 @@ const DOM = {
       .getElementById("totalDisplay")
       .innerHTML = Utils.formatCurrency(Transaction.total());
   },
-  clearTransaction(){
+  clearTransaction() {
     DOM.transactionContainer.innerHTML = "";
   }
 }
 
 const Utils = {
-  formatAmount(value){
+  formatAmount(value) {
     value = value * 100;
     return Math.round(value);
   },
-  formatDate(date){
+  formatDate(date) {
     const splittedDate = date.split("-");
     return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
   },
-  reformatDate(date){
+  reformatDate(date) {
     const splittedDate = date.split("/");
     return `${splittedDate[2]}-${splittedDate[1]}-${splittedDate[0]}`;
   },
-  formatCSV(data){
-    
+  formatCSV(data) {
+
     const headers = Object.keys(data[0]);
     const formatedCSV = [headers]
-     data.forEach(row => {
+    data.forEach(row => {
       let values = headers.map(header => {
-          const escaped =  `${String(row[header]).replace(/"/g, '')}`
-          return escaped
-        })
+        const escaped = `${String(row[header]).replace(/"/g, '')}`
+        return escaped
+      })
       formatedCSV.push(values.join(','))
     });
-    
+
     return formatedCSV.join('\n');
   },
-  formatCurrency(value){
-    const signal = Number(value) < 0? "- " : "";
-    
+  formatCurrency(value) {
+    const signal = Number(value) < 0 ? "- " : "";
+
     value = String(value).replace(/\D/g, "");
-    value = Number(value)/100;
+    value = Number(value) / 100;
     value = value.toLocaleString("pt-BR", {
       style: "currency",
       currency: "BRL"
@@ -192,8 +192,8 @@ const Utils = {
 
     return signal + value;
   },
-  formatData(value){
-    value = Number(value)/100;
+  formatData(value) {
+    value = Number(value) / 100;
     return value;
   }
 }
@@ -202,125 +202,126 @@ const Form = {
   description: document.querySelector('input#description'),
   amount: document.querySelector('input#amount'),
   date: document.querySelector('input#date'),
-  handleBlankFields(error){
+  handleBlankFields(error) {
     const errorBox = document.querySelector(".error-box")
     errorBox.classList.add('active');
     errorBox.innerText = error.message;
   },
-  getValues(){
+  getValues() {
     return {
       description: Form.description.value,
       amount: Form.amount.value,
       date: Form.date.value
     }
   },
-  validateFields(){
-    const {description, amount, date} = Form.getValues();
-    if(description.trim() === "" || amount.trim() === "" || date.trim() === ""){
+  validateFields() {
+    const { description, amount, date } = Form.getValues();
+    if (description.trim() === "" || amount.trim() === "" || date.trim() === "") {
       throw new Error("Por favor, preencha todos os campos!")
     }
   },
-  formatData(){
-    let {description, amount, date} = Form.getValues();
+  formatData() {
+    let { description, amount, date } = Form.getValues();
     amount = Utils.formatAmount(amount);
     date = Utils.formatDate(date);
 
-    return{
+    return {
       description,
       amount,
       date
     }
   },
-  clearFields(){
+  clearFields() {
     Form.description.value = "";
     Form.amount.value = "";
     Form.date.value = "";
   },
-  submit(event){
+  submit(event) {
     event.preventDefault();
-    
-    try{
+
+    try {
       Form.validateFields();
       const transaction = Form.formatData();
 
-      Modal.isEdit ? 
+      Modal.isEdit ?
         Transaction.saveEdit(transaction, Transaction.underEditionIndex) :
         Transaction.add(transaction);
 
       Form.clearFields();
       Modal.close();
 
-    } catch(error){
+    } catch (error) {
       Form.handleBlankFields(error);
     }
   }
 }
 
-const Graph =  {
-  draw(){  
-    const graph = document.getElementById("chart").getContext("2d");
-    const myChart = new Chart(graph, {
-      type: "bar",
-      data: {
-        labels: Graph.getLabels(),
-        datasets: [
+const Graph = {
+  hook: document.getElementById("chart").getContext("2d"),
+  constructor: {
+    type: "bar",
+    data: {
+      labels: [],
+      datasets: [
+        {
+          label: "Balance",
+          data: [],
+          backgroundColor: []
+        },
+      ],
+    },
+    options: {
+      scales: {
+        xAxes: [
           {
-            label: "Balance",
-            data: Graph.getData(),
-            backgroundColor: Graph.getColors()
+            gridLines: {
+              display: false,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
           },
         ],
       },
-      options: {
-        scales: {
-          xAxes: [
-            {
-              gridLines: {
-                display: false,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              gridLines: {
-                display: false,
-              },
-            },
-          ],
-        },
-      },
-    });
-
-    return myChart;
+    },
   },
-  getLabels(){
+  getLabels() {
     const list = [];
     Storage.get().forEach(element => {
       list.push(element.description)
     });
-    return list;
+    App.chart.data.labels = list;
   },
-  getData(){
+  getData() {
     const data = [];
     Storage.get().forEach(element => {
       data.push(Utils.formatData(element.amount))
     });
-    return data;
+    App.chart.data.datasets[0].data = data
   },
-  getColors(){
+  getColors() {
     const colors = [];
     Storage.get().forEach(element => {
-      colors.push( element.amount > 0? '#12A454' : '#e92929');
-      
+      colors.push(element.amount > 0 ? '#12A454' : '#e92929');
     });
-    return colors;
+    App.chart.data.datasets[0].backgroundColor = colors;
+  },
+  updateData() {
+    Graph.getLabels();
+    Graph.getData();
+    Graph.getColors();
+    App.chart.update();
   }
 }
 
 const Export = {
-  downloadCSV(){     
+  downloadCSV() {
     const csvToExport = Utils.formatCSV(Storage.get());
-    const blob = new Blob([csvToExport], {type: 'text/csv'});
+    const blob = new Blob([csvToExport], { type: 'text/csv' });
     const url = window.URL.createObjectURL(blob);
 
     const downloadLink = document.createElement('a');
@@ -336,64 +337,70 @@ const Export = {
       .classList.toggle('hide')
     Modal.close();
   },
-  openLinkModal(){
+  openLinkModal() {
     document
-    .querySelector(".modal-overlay")
-    .classList
-    .add("active");
+      .querySelector(".modal-overlay")
+      .classList
+      .add("active");
     document
       .querySelector('#form')
-        .classList.add('hide')
+      .classList.add('hide')
     document.querySelector('.downloadbox')
       .classList.remove('hide')
   }
 }
-  
+
 const Mode = {
   screen: document.documentElement,
-  alternate(){
-    const toggleControl = document.querySelector('#switch');   
+  alternate() {
+    const toggleControl = document.querySelector('#switch');
     Mode.transition();
-    toggleControl.checked ? 
-      Mode.screen.setAttribute('data-theme', 'dark') : 
+    toggleControl.checked ?
+      Mode.screen.setAttribute('data-theme', 'dark') :
       Mode.screen.setAttribute('data-theme', 'light');
-    
-    Mode.saveMode();      
+
+    Mode.saveMode();
   },
-  transition(){
+  transition() {
     Mode.screen.classList.add('transition');
     window.setTimeout(() => {
       Mode.screen.classList.remove('transition');
     }, 3000);
   },
-  onWakeMode(){
+  onWakeMode() {
     const currentMode = Storage.getMode();
     const toggleControl = document.querySelector('#switch');
 
     toggleControl.defaultChecked = currentMode == "dark";
 
-    if(currentMode == "dark"){
+    if (currentMode == "dark") {
       Mode.screen.setAttribute('data-theme', 'dark');
     }
   },
-  saveMode(){
+  saveMode() {
     const currentMode = Mode.screen.getAttribute('data-theme');
     Storage.setMode(currentMode);
   }
 }
-  
+
 const App = {
-  init(){
+  chart: null,
+  isFirstInitializaiton: true,
+  init() {
     Mode.onWakeMode();
     Transaction.all.forEach(DOM.addTransaction);
-    DOM.updateBalance();  
+    DOM.updateBalance();
     Storage.set(Transaction.all);
-    Graph.draw();
+    if (App.isFirstInitializaiton) {
+      App.chart = new Chart(Graph.hook, Graph.constructor);
+    }
+    Graph.updateData();
   },
-  reload(){
+  reload() {
+    App.isFirstInitializaiton = false
     DOM.clearTransaction();
     App.init();
   }
 }
 
-  App.init()
+App.init()
